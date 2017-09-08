3f83679129ab057f3a7bbbc807bf2b092bfd2a1a
Jin Cheng <jincheng88@outlook.com>
2017-09-07 21:13:58 -0500

    created maven spring project

---
+++ src/main/java/com/learningjava/SpringCatalogApiApplication.java
@@ -0,0 +1,12 @@
+package com.learningjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCatalogApiApplication.class, args);
+	}
+}
